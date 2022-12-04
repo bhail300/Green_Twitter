93b58307892bcf00a53f30d265ba514bca2d8f8f
bhail300 <bhaile300@gmail.com>
2022-12-04 00:15:32 -0800

    added image file

---
+++ green_twitter/pages/comps/images.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Image } from 'semantic-ui-react'
+
+const ImageExampleImage = () => (
+  <Image src='https://pomf2.lain.la/f/m30iygc1.png' size='small' />
+)
+
+export default ImageExampleImage
