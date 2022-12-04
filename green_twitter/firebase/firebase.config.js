ff01d2b38c3ac290077e5f2f6c65c565a0ed85ff
bhail300 <bhaile300@gmail.com>
2022-12-03 20:44:49 -0800

    added firebase config file

---
+++ green_twitter/firebase/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDF8qHFzY1eecYGJ30maob0fF6yABbhtBc",
+  authDomain: "green-aecb9.firebaseapp.com",
+  projectId: "green-aecb9",
+  storageBucket: "green-aecb9.appspot.com",
+  messagingSenderId: "120896384878",
+  appId: "1:120896384878:web:13f7fadb8a557e7bfbdb7f"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
